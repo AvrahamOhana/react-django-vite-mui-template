01c7d445cbd7176b8a46985122dde67b13299b91
AvrahamOhana <amiko617@gmail.com>
2024-02-11 23:25:13 +0200

    add collapsable navbar

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,20 +1,20 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
-import theme from './theme';
-
+import { BrowserRouter as Router } from 'react-router-dom'
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
+  <Router>
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
       <CssBaseline />
       <App />
-    </ThemeProvider>
-    ,
-  </React.StrictMode>,
+    
+    
+  </React.StrictMode>
+  </Router>
 );
